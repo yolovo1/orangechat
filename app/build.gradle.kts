c923b7f4d0fe1af969ecb5291ae6fb37df5ba452
yolovo1 <2175310092@qq.com>
2026-07-21 02:34:42 +0800

    bump version to 2.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 160
-        versionName = "2.2.4"
+        versionName = "2.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
